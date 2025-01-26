11e18c65fa1f53c787078e8d4e79e3995890d842
Volodymyr999 <voloduya204@gmail.com>
2025-01-26 15:14:21 +0200

    5  commit first branch

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-Changes in first branch
-=======
 first branch text
 first branch text2
 first branch text3
 first branch text4
 first branch text5
 first branch text6
->>>>>>> first_branch
