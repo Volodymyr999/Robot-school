eb17c06bfe0759d4d2cee6de255bec665bedb67c
Volodymyr999 <voloduya204@gmail.com>
2025-01-26 19:37:13 +0200

    remowing half of text

--- index.js
+++ index.js
@@ -12,23 +12,3 @@
  branch text2
  branch text3
  branch text4
-
-first branch text
-first branch text2
-first branch text3
-first branch text4
-
-first branch text
-first branch text2
-first branch text3
-first branch text4
-
-first branch text
-first branch text2
-first branch text3
-first branch text4
-
-first branch text
-first branch text2
-first branch text3
-first branch text4
