166a132a1366900ead8a9282989ec73ca0d8159b
Volodymyr999 <voloduya204@gmail.com>
2025-01-26 18:22:30 +0200

    remowing 2 more blocks

--- index.js
+++ index.js
@@ -7,13 +7,3 @@ first branch text
 first branch text2
 first branch text3
 first branch text4
-
-first branch text
-first branch text2
-first branch text3
-first branch text4
-
-first branch text
-first branch text2
-first branch text3
-first branch text4
