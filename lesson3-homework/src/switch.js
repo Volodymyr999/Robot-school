f14e7e33fcb2bc66e44f60ea458a60967b198567
Volodymyr999 <voloduya204@gmail.com>
2025-02-02 16:53:27 +0200

    task4

---
+++ lesson3-homework/src/switch.js
@@ -0,0 +1,16 @@
+let num1 = 7;
+let num2 = 5;
+let num3 = 30;
+let num4 = 20;
+
+switch(true) {
+case(num1 < num2):
+    console.log("condition1");
+    break;
+case(num3 >= num4 && num2 > num1):
+    console.log("condition2");
+    break;
+default:
+    console.log("condition3");
+    break;
+}
