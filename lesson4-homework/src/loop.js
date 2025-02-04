34e085842536d3fb347fe40928489ba3e29ece39
Volodymyr999 <voloduya204@gmail.com>
2025-02-05 00:20:33 +0200

    rework(adding WHILE and DO WHILE )

--- lesson4-homework/src/loop.js
+++ lesson4-homework/src/loop.js
@@ -2,8 +2,27 @@ for (let i = 0; i < 10; i = i +1) {
     console.log(i);
 }
 
-console.log("----------");
+console.log("FOR function");
 
 for (let i = 100; i >= 0; i -= 10) {
     console.log(i);
 }
+
+
+console.log("while");
+
+
+let i = 0;
+while (i<7) {
+    console.log(i);
+    i ++;
+};
+
+console.log("do-while");
+
+let a = 6;
+do {
+    console.log(a);
+    a ++;
+}
+while (a < 12);
