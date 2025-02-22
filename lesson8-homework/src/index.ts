eeb75f62d6d211f1eb79781f71e7891a517a9fcc
Volodymyr999 <voloduya204@gmail.com>
2025-02-22 19:28:46 +0200

    minor fixes

--- lesson8-homework/src/index.ts
+++ lesson8-homework/src/index.ts
@@ -1,20 +1,19 @@
 import { Car, Bicycle } from './abstraction';
-import { getJson } from './function';
-import { getJson3 } from './function';
+import { getJson, getJson3 } from './function';
 import { User, Geo, Address, Company } from './class';
 
 
 console.log('Abstraction class Vehicle work below');
 //викристання калсів Car, Bicycle
 
-const myCar = new Car("Toyota", "Corolla", "V6");
-const ownCar = new Car("BMW", "5 series", "L6");
+const myCar = new Car('Toyota', 'Corolla', 'V6');
+const ownCar = new Car('BMW', '5 series', 'L6');
 ownCar.start();
 myCar.start(); // Starting Toyota Corolla. Engine (V6) started.
 myCar.accelerate(); // Toyota Corolla is accelerating.
 myCar.stop(); // Toyota Corolla has stopped.
 
-const myBicycle = new Bicycle("Trek", "FX 2");
+const myBicycle = new Bicycle('Trek', 'FX 2');
 myBicycle.start(); // Pedaling Trek FX 2.
 myBicycle.ringBell(); // Trek FX 2 is ringing the bell.
 myBicycle.stop(); // Trek FX 2 has stopped.і
@@ -28,12 +27,13 @@ myBicycle.stop(); // Trek FX 2 has stopped.і
     console.log(data.name);
     console.log(data.address.geo.lat);
     console.log('--------------');
-    const geo = new Geo("-37.3159", "81.1496");
-    const address = new Address("Kulas Light", "Apt. 556", "Gwenborough", "92998-3874", geo);
-    const company = new Company("Delasport", "Multi-layered client-server neural-net", "harness real-time e-markets");
-    const user = new User(1, "Volodymyr", "Sport", "Sincere@april.biz", address, "1-770-736-8031 x56442", "hildegard.org", company);
+    const geo = new Geo('-37.3159', '81.1496');
+    const address = new Address('Kulas Light', 'Apt. 556', 'Gwenborough', '92998-3874', geo);
+
+    const company = new Company('Delasport', 'Multi-layered client-server neural-net', 'harness real-time e-markets');
+    const user = new User(1, 'Volodymyr', 'Sport', 'Sincere@april.biz', address, '1-770-736-8031 x56442', 'hildegard.org', company);
     user.displayInfo();
-    const geo2 = new Geo("-37", "81");
+    const geo2 = new Geo('-37', '81');
     console.log(geo2);
 })();
 
@@ -45,4 +45,3 @@ myBicycle.stop(); // Trek FX 2 has stopped.і
     console.log(data.name);
     console.log(data.address.geo.lat);
 })();
-
